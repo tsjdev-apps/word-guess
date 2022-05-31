5b32d4eab740178bea134028ba826d01e9890709
Sebastian Jensen <apps@tsjdev-apps.de>
2022-05-31 06:30:58 +0200

    Add WordGuess project.

---
+++ WordGuess/WordGuess/Program.cs
@@ -0,0 +1,11 @@
+﻿using Spectre.Console.Cli;
+using WordGuess.Commands;
+
+var app = new CommandApp();
+
+app.Configure(config =>
+{
+    config.AddCommand<WordGuessCommand>("word-guess");
+});
+
+return await app.RunAsync(args);
